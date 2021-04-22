e07ed6c6caedbb94673fda8db81f81ac6c6eacff
Flufflul <mufflul@gmail.com>
2021-04-22 15:50:28 -0400

    Add BGM loop

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Water")) {
+            audioSource.PlayOneShot(splashSound);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+       if (other.CompareTag("Water")) {
+            audioSource.PlayOneShot(splashSound);
+        }
+    }
+}
