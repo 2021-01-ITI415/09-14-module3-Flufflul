8d9bcbab476eaee7b9abac6244cbdfbfc0b4aabb
Flufflul <mufflul@gmail.com>
2021-05-01 20:57:19 -0400

    Add note + counter

---
+++ Assets/_Labs/07-Prototype3/Scripts/NoteCollect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteCollect : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("NoteCollectible")) {
+            Debug.Log("Note found.");
+
+            other.gameObject.SetActive(false);
+            StatController.notes++;
+            StatController.S.updateStats();
+
+            StatController.debugCollect();
+        } 
+        else {
+            Debug.Log("Untagged trigger");
+        }
+    }
+}
---
+++ Assets/_Labs/07-Prototype3/Scripts/StatController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatController : MonoBehaviour
+{
+    public static StatController    S;
+    public static int               notes,
+                                    totalNotes;
+    
+    public Text                     text_noteCount;
+
+    private void Start() {
+        if (S == null) S = this;
+
+        notes = 0;
+
+        GameObject[] collectibles = GameObject.FindGameObjectsWithTag("NoteCollectible");
+        totalNotes = collectibles.Length;
+
+        updateStats();
+    }
+
+    public void updateStats() {
+        string noteCount = notes + "/" + totalNotes;
+        text_noteCount.text = noteCount;
+    }
+
+    public static void debugCollect() {
+        Debug.Log("Collected notes: " + notes + "/" + totalNotes);
+    }
+}
